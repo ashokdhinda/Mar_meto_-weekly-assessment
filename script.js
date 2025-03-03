e46b5ac23114473150d263d6d2f3c63809e427b7
ashokdhinda <ashokkudhinda143@gmail.com>
2025-03-04 02:05:40 +0530

    update the file and all done

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function fetchProducts() {
     try {
-      const response = await fetch("https://fakestoreapi.com/products");
+      const response = await fetch("./products.json");
       products = await response.json();
       displayProducts(products);
     } catch (error) {
